827d6f2ca24862cd5fe1d69eb1a3eb23ee07e0bb
desparta05 <desparta05@gmail.com>
2021-03-25 00:24:37 -0500

    Completed final changes to the models index.js

--- models/Songs.js
+++ models/Songs.js
@@ -5,62 +5,62 @@ class Songs extends Model {}
 
 Songs.init(
     {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    // song_request: {
-    //     type: DataTypes.STRING,
-    //     allowNull: false,
-    //     validate: {
-    //         len: [8],
-    //     },
-    // },
-    artist_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        // validate: {
-        //     len: [8],
-        // },    
-    },
-    year_released: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-    // time: {
+    // id: {
     //     type: DataTypes.INTEGER,
     //     allowNull: false,
+    //     primaryKey: true,
+    //     autoIncrement: true,
     // },
-    playlist_id: {
-        type: DataTypes.INTEGER,
-        references: {
-        model: 'playlist',
-        key: 'id',
-        },
-    },
-    artist_id: {
-        type: DataTypes.INTEGER,
-        references: {
-        model: 'artist',
-        key: 'id',
-        },
-    },
-    library_id: {
-        type: DataTypes.INTEGER,
-        references: {
-        model: 'library',
-        key: 'id',
-        },
-    },
-    search_id: {
-        type: DataTypes.INTEGER,
-        references: {
-        model: 'search',
-        key: 'id',
-        },
-    },
+    // // song_request: {
+    // //     type: DataTypes.STRING,
+    // //     allowNull: false,
+    // //     validate: {
+    // //         len: [8],
+    // //     },
+    // // },
+    // artist_name: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false,
+    //     // validate: {
+    //     //     len: [8],
+    //     // },    
+    // },
+    // year_released: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: true,
+    // },
+    // // time: {
+    // //     type: DataTypes.INTEGER,
+    // //     allowNull: false,
+    // // },
+    // playlist_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //     model: 'playlists',
+    //     key: 'id',
+    //     },
+    // },
+    // artist_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //     model: 'artist',
+    //     key: 'id',
+    //     },
+    // },
+    // library_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //     model: 'library',
+    //     key: 'id',
+    //     },
+    // },
+    // search_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //     model: 'search',
+    //     key: 'id',
+    //     },
+    // },
 },
     {
     sequelize,
