1f040adcc8d1a9b84164ce69d7bbf9f078e06d1c
Joseph Soria <josephsoria6@gmail.com>
2021-03-26 21:22:27 -0500

    made /album /artist /song post routes

--- controllers/api/libraryRoutes.js
+++ controllers/api/libraryRoutes.js
@@ -8,26 +8,24 @@ router.get('/', (req, res) => {
   }).then((Category) => res.json(Category));
 })
 
-router.get('/:id', (req, res) => {
-  libraryRoutes.findByPk(req.params.id).then((libraryRoutes) => res.json(libraryRoutes));
-});
-
-
-
-router.post('/', async (req, res) => {
+router.post('/artist', async (req, res) => {
   try {
-    const newlibraryRoutes = await libraryRoutes.create({
-      ...req.body,
-      user_id: req.session.user_id,
+    const artistData = await Artist.create({
+      artist_name: req.body.artist_name,
+      artist_songs: req.body.artist_songs,
+      artist_image: req.body.artist_image,
+      artist_id: req.body.artist_id,
+      library_id: req.session.libraryId,
     });
-
-    res.status(200).json(newlibraryRoutes);
+  
+    res.status(200).json(artistData);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
 
-router.post('/music', async (req, res) => {
+router.post('/album', async (req, res) => {
   try {
     const albumsData = await Albums.create({
       album_title: req.body.album_title,
@@ -36,15 +34,20 @@ router.post('/music', async (req, res) => {
       artist_id: req.body.artist_id,
       library_id: req.session.libraryId,
     });
-    const artistData = await Artist.create({
-      artist_name: req.body.artist_name,
-      artist_songs: req.body.artist_songs,
-      artist_image: req.body.artist_image,
-      artist_id: req.body.artist_id,
-      library_id: req.session.libraryId,
-    });
+
+    res.status(200).json(albumsData);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.post('/song', async (req, res) => {
+  try {
+
     const songsData = await Songs.create({
       song_name: req.body.song_name,
+      song_id: req.body.song_id,
       artist_name: req.body.artist_name,
       artist_songs: req.body.artist_songs,
       artist_id: req.body.artist_id,
@@ -52,11 +55,10 @@ router.post('/music', async (req, res) => {
     });
     res.status(200).json(albumsData);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
-
-
 
 router.delete('/:id', async (req, res) => {
   try {
